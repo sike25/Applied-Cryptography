9c946e9fb620896e5fe72ab0d10ac7d717d566cf
sike25 <97693483+sike25@users.noreply.github.com>
2024-03-08 16:00:10 +0100

    Update PO_attack_skeleton.py

--- block again aes/padding oracle/PO_attack_skeleton.py
+++ block again aes/padding oracle/PO_attack_skeleton.py
@@ -3,11 +3,11 @@ from oracle import oracle
 PADDING_ERROR = True
 PADDING_OK = False
 
-__TODO_1__ = b'\x00'
-__TODO_2__ = b'\x00'
-__TODO_3__ = 0
-__TODO_4__ = 0
-__TODO_5__ = 0
+# __TODO_1__ = b'\x00' * 16
+# __TODO_2__ = R + Y
+# __TODO_3__ = 15 - i
+# __TODO_4__ = 16 - i
+# __TODO_5__ =  R[16 - plen] ^ P[0]
 
 # This is the ciphertext block that we want to break
 Y_hex = '5f8043943189c3a3c3e6bd0d2237f73f'
@@ -20,8 +20,8 @@ Y = bytes.fromhex(Y_hex)
 # We change the last byte of the controlled block R until we get correct padding
 # (the other bytes of the controlled block can be anything, for instance, all bytes can be 0)
 
-R = __TODO_1__ # recall that the AES block size is 16 bytes
-while oracle(__TODO_2__) == PADDING_ERROR: 
+R = b'\x00' * 16 # recall that the AES block size is 16 bytes
+while oracle(R + Y) == PADDING_ERROR: 
     r = (R[-1]+1).to_bytes(1, 'big') # value of the last byte of R is incremented
     R = R[:-1]+r                     # and we put it back into the controlled block R
 
@@ -30,7 +30,7 @@ while oracle(__TODO_2__) == PADDING_ERROR:
 # So we determine the padding length here
 
 for i in range(16): # We will change every byte in R, until we get a padding error again
-    S = R[:__TODO_3__] + b'\xFF' + R[__TODO_4__:]  # We implement the byte change by simply setting the i-th byte of R to FF
+    S = R[:i] + b'\xFF' + R[i+1:]  # We implement the byte change by simply setting the i-th byte of R to FF
     if oracle(S+Y) == PADDING_ERROR: # When we get an error, the just modified byte must be the first padding byte
         plen = 16-i                  # so essentially we can determine the padding length plen
         break
@@ -45,15 +45,17 @@ for i in range(plen):                           # We XOR the padding P to the la
     X += (P[i]^R[-plen+i]).to_bytes(1, 'big')   # and this gives the last bytes of the plaintext X
 
 # We determine the remaining missing bytes iteratively...
-
 while plen < 16: # In each iteration, we will determine 1 more byte out of the missing (16 - plen) bytes
     R = R[:16-plen] + (R[16-plen]^0x80).to_bytes(1, 'big') + R[16-plen+1:]  # We set byte 16-plen of R such that 
                                                                             # in the resulting padding byte 16-plen be x00
     while oracle(R+Y) == PADDING_ERROR:
         r = (R[16-plen-1]+1).to_bytes(1, 'big')     # Then we increment byte 16-plen-1 of R 
         R = R[:16-plen-1] + r + R[16-plen:]         # until we get a correct padding again
-    X = (__TODO_5__).to_bytes(1, 'big') + X  # This allows for determining a new byte of X
+    
+    # because the padding is okay, it is very likely this byte is 80
+    X = (R[16-plen-1] ^ P[0]).to_bytes(1, 'big') + X  # This allows for determining a new byte of X
     plen += 1
 
+
 # We are done: we decoded the entire plaintext block X
-print("Plaintext block recovered: " + X.hex() + " (ASCII: " + X.decode('ascii') + ")")
+print("Plaintext block recovered: " + X.hex() + " (ASCII: " + X.decode('ascii', errors='ignore') + ")")
